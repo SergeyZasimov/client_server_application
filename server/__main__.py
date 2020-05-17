65ecacd1ccf76ba18070b0e3e7963a9bec392d4c
SergeyZasimov <zasimov.ser@yandex.ru>
2020-05-17 10:54:46 +0300

    Оформлено журналирование

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import socket
+import logging
 from argparse import ArgumentParser
 
 from resolvers import find_server_action
@@ -32,18 +33,35 @@ host = args.host if args.host else config.get('host')
 port = args.port if args.port else config.get('port')
 buffersize = config.get('buffersize')
 
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('server.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
 try:
     sock = socket.socket() # создать сокет
     sock.bind((host, port)) # привязать сокет к IP-адресу и порту сервера 
     sock.listen(5) # готовность принимать соединение
 
-    print(f'Server started with {host}:{port}')
+    logger.info(f'Server started with {host}:{port}')
 
     action_mapping = find_server_action()
 
     while True:
         client, (client_host, client_port) = sock.accept() # принять запрос на установку соединения
-        print(f'Client {client_host}:{client_port} was connected')
+        logger.info(f'Client {client_host}:{client_port} was connected')
 
         bytes_request = client.recv(buffersize) # принять данные
 
@@ -55,18 +73,19 @@ try:
             if controller:
                 try:
                     response = controller(request)
-                    print(f'Request: {bytes_request.decode()}')
+                    logger.debug(f'Request: {bytes_request.decode()}')
                 except Exception as err:
                     response = make_500(request)
-                    print(err)
+                    logger.critical(err)
             else:
                 response = make_404(request)
+                logger.error(f'Action with name {action} not found')
         else:
             response = make_404(request)
-            print(f'Wrong request: {request}')
+            logger.error(f'Wrong request: {request}')
 
         string_response = json.dumps(response)
         client.send(string_response.encode())
         client.close()
 except KeyboardInterrupt:
-    print("Server shutdown")
+    logger.info("Server shutdown")
