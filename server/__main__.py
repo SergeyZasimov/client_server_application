3da22fc3bd20125f1b2e41407135142f33212f0e
SergeyZasimov <zasimov.ser@yandex.ru>
2020-05-17 12:33:45 +0300

    Декорирование логирования

--- server/__main__.py
+++ server/__main__.py
@@ -33,35 +33,27 @@ host = args.host if args.host else config.get('host')
 port = args.port if args.port else config.get('port')
 buffersize = config.get('buffersize')
 
-logger = logging.getLogger('main')
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-file_handler = logging.FileHandler('server.log')
-file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.DEBUG)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-stream_handler.setLevel(logging.DEBUG)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=(
+        logging.FileHandler('server.log'),
+        logging.StreamHandler(),
+    )
+)
 
 try:
     sock = socket.socket() # создать сокет
     sock.bind((host, port)) # привязать сокет к IP-адресу и порту сервера 
     sock.listen(5) # готовность принимать соединение
 
-    logger.info(f'Server started with {host}:{port}')
+    logging.info(f'Server started with {host}:{port}')
 
     action_mapping = find_server_action()
 
     while True:
         client, (client_host, client_port) = sock.accept() # принять запрос на установку соединения
-        logger.info(f'Client {client_host}:{client_port} was connected')
+        logging.info(f'Client {client_host}:{client_port} was connected')
 
         bytes_request = client.recv(buffersize) # принять данные
 
@@ -73,19 +65,19 @@ try:
             if controller:
                 try:
                     response = controller(request)
-                    logger.debug(f'Request: {bytes_request.decode()}')
+                    logging.debug(f'Request: {bytes_request.decode()}')
                 except Exception as err:
                     response = make_500(request)
-                    logger.critical(err)
+                    logging.critical(err)
             else:
                 response = make_404(request)
-                logger.error(f'Action with name {action} not found')
+                logging.error(f'Action with name {action} not found')
         else:
             response = make_404(request)
-            logger.error(f'Wrong request: {request}')
+            logging.error(f'Wrong request: {request}')
 
         string_response = json.dumps(response)
         client.send(string_response.encode())
         client.close()
 except KeyboardInterrupt:
-    logger.info("Server shutdown")
+    logging.info("Server shutdown")
--- server/echo/controllers.py
+++ server/echo/controllers.py
@@ -1,4 +1,6 @@
 from protocol import make_200
+from decorators import logged
 
+@logged('request: %(request)s - response: %(result)s')
 def echo_controller(request):
     return make_200(request, request.get('data'))
